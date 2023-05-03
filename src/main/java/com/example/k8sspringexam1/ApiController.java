78aa3f821ca06cd0f56f3d7ec4ff0e71151969dc
hanumoka-acer <amagramer@gmail.com>
2023-05-03 11:50:48 +0900

    1

--- src/main/java/com/example/k8sspringexam1/ApiController.java
+++ src/main/java/com/example/k8sspringexam1/ApiController.java
@@ -3,6 +3,7 @@ package com.example.k8sspringexam1;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -10,9 +11,12 @@ import java.net.UnknownHostException;
 public class ApiController {
 
     @GetMapping("/")
-    public String hello() throws UnknownHostException {
+    public String hello(HttpServletRequest request) throws UnknownHostException {
         InetAddress inetAddress = InetAddress.getLocalHost();
         long pid = ProcessHandle.current().pid();
-        return "Hello World! G This is a k8s spring boot exam1 IP:" + inetAddress.getHostAddress() +"pid:"+ pid;
+        // reqeust에서 요청자 ip 추출
+        System.out.println("remoteAddr:" + request.getRemoteAddr());
+        System.out.println("pid:" + pid + " IP:" + inetAddress.getHostAddress());
+        return "Hello World! G This is a k8s spring boot exam1 IP:" + inetAddress.getHostAddress() + "pid:" + pid;
     }
 }
